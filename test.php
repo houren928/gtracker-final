014a18e8e180e2d84565163a15f4a823ffd2b3e5
Hou Ren <houren928@gmail.com>
2022-06-19 23:30:01 +0800

    delete unused

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php 
-date_default_timezone_set("Asia/Kuala_lumpur");
-// echo date_default_timezone_get();
-$dt = new DateTime();
-        $today = $dt->format('Y-m-d H:i:s');
-        echo $today;
-?>
