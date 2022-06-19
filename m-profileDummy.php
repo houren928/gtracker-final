88491e52e3bc8984a4df207eea2606e280b1c62d
Hou Ren <houren928@gmail.com>
2022-06-19 23:33:39 +0800

    delete html

--- m-profileDummy.php
+++ m-profileDummy.php
@@ -122,9 +122,6 @@ $resultDelete = mysqli_query($conn, "SELECT * FROM user WHERE user_id =  $id");
                                     </form>
                                 </div>
                             </li>
-                            <!-- <li class="nav-item dropdown no-arrow mx-1">
-                                <?php include_once "alert-button-process.php"; ?>
-                            </li> -->
                             <div class="d-none d-sm-block topbar-divider"></div>
                             <li class="nav-item dropdown no-arrow">
                                 <div class="nav-item dropdown show no-arrow">
